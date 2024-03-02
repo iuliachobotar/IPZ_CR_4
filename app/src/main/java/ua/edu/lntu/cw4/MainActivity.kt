cb04e14172785e9c9dc36f1f1ca21065d43aa0f0
iuliachobotar <chobotar_ly@ukr.net>
2024-03-02 13:05:40 +0200

    list

--- app/src/main/java/ua/edu/lntu/cw4/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/cw4/MainActivity.kt
@@ -3,13 +3,18 @@ package ua.edu.lntu.cw4
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import ua.edu.lntu.cw4.ui.theme.IPZ_CR_4Theme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +22,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             IPZ_CR_4Theme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                        ListScreen()
+                    }
                 }
             }
         }
@@ -30,17 +36,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun ListScreen() {
+    val data = listOf("Елемент 1", "Елемент 2", "Елемент 3", "Елемент 4", "Елемент 5")
+
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(16.dp) // Встановлюємо відступ між елементами
+    ) {
+        items(data) { item ->
+            ListItem(text = item)
+        }
+    }
+}
+
+@Composable
+fun ListItem(text: String) {
     Text(
-        text = "Heeeeeeeeelo $name!",
-        modifier = modifier
+        text = text,
+        fontSize = 28.sp, // Розмір шрифту 28
+        modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp) // Змінюємо відступи
     )
 }
 
+
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ListScreenPreview() {
     IPZ_CR_4Theme {
-        Greeting("Android")
+        ListScreen()
     }
 }
